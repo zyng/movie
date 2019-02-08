5e5e1ee3e3c1e4f56f4a4eb369bd673d176aebe3
zyng9 <kisielewskimichal@gmail.com>
2019-02-08 14:59:54 +0100

    change unexpected-error message on mobile

--- src/components/Errors/SomethingWrong.js
+++ src/components/Errors/SomethingWrong.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SomeThingWrong = () => {
     return (
-        <div className="unexpected-error">The service is temporarily unavailable. We apologize and invite you soon. </div>
+        <div className="unexpected-error"><p>The service is temporarily unavailable. We apologize and invite you soon. </p></div>
     );
 }
 
